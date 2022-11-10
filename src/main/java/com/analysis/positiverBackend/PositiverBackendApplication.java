e2c7c7691789d458762e9934af8d3ac7c6847e07
schou35 <schou35@gatech.edu>
2022-11-10 04:17:05 -0500

    first commit

---
+++ src/main/java/com/analysis/positiverBackend/PositiverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.analysis.positiverBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositiverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositiverBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analysis/positiverBackend/PositiverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analysis.positiverBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositiverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
